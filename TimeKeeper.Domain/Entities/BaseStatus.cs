811161798a1c7774b5448a4f4ef11f961e797605
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-10-14 16:15:13 +0200

    Initial commit

---
+++ TimeKeeper.Domain/Entities/BaseStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeKeeper.Domain.Entities
+{
+    public class BaseStatus :BaseClass
+    {
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
