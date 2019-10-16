8daf03b5128c574bd30b6921716999e3c6e8cb65
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-10-16 09:55:14 +0200

    Fixed problems

--- TimeKeeper.DAL/UnitOfWork.cs
+++ TimeKeeper.DAL/UnitOfWork.cs
@@ -9,20 +9,20 @@ namespace TimeKeeper.DAL
     public class UnitOfWork : IDisposable
     {
         private readonly TimeKeeperContext _context;
-        private Repository<Project> _projects;
-        private Repository<PricingStatus> _pricingstatuses;
-        private Repository<Customer> _customers;
-        private Repository<CustomerStatus> _customerstatuses;
-        private Repository<Day> _calendar;
-        private Repository<DayType> _daytypes;
-        private Repository<Employee> _employees;
-        private Repository<EmployeePosition> _employeepositions;
-        private Repository<EmploymentStatus> _employmentstatuses;
-        private Repository<JobDetail> _tasks;
-        private Repository<Member> _members;
-        private Repository<ProjectStatus> _projectstatuses;
-        private Repository<Role> _roles;
-        private Repository<Team> _teams;
+        private IRepository<Project> _projects;
+        private IRepository<PricingStatus> _pricingStatuses;
+        private IRepository<Customer> _customers;
+        private IRepository<CustomerStatus> _customerstatuses;
+        private IRepository<Day> _calendar;
+        private IRepository<DayType> _daytypes;
+        private IRepository<Employee> _employees;
+        private IRepository<EmployeePosition> _employeepositions;
+        private IRepository<EmploymentStatus> _employmentstatuses;
+        private IRepository<JobDetail> _tasks;
+        private IRepository<Member> _members;
+        private IRepository<ProjectStatus> _projectstatuses;
+        private IRepository<Role> _roles;
+        private IRepository<Team> _teams;
 
         //Will the context injection here necessary?
         public UnitOfWork(TimeKeeperContext context=null)
