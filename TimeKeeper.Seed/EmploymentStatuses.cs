53f0d7118025ff1d88969cbe05a964bb2903cc3c
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-10-16 23:47:45 +0200

    New files for seeding database added

---
+++ TimeKeeper.Seed/EmploymentStatuses.cs
@@ -0,0 +1,27 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeKeeper.DAL;
+using TimeKeeper.Domain.Entities;
+
+namespace TimeKeeper.Seed
+{
+    public class EmploymentStatuses
+    {
+        public static void Collect(ExcelWorksheet rawData, UnitOfWork unit)
+        {
+            for (int row = 2; row <= rawData.Dimension.Rows; row++)
+            {
+                int oldId = rawData.ReadInteger(row, 1);
+                EmploymentStatus employmentStatus = new EmploymentStatus
+                {
+                    Name = rawData.ReadString(row, 2)
+                };
+                unit.EmploymentStatuses.Insert(employmentStatus);
+                unit.Save();
+                Utility.employmentStatusesDictionary.Add(oldId, employmentStatus.Id);
+            }
+        }
+    }
+}
--- TimeKeeper.Seed/Program.cs
+++ TimeKeeper.Seed/Program.cs
@@ -9,18 +9,29 @@ namespace TimeKeeper.Seed
     {
         static void Main(string[] args)
         {
+            FileInfo fileStatuses = new FileInfo(@"C:\TimeKeeper\TimeKeeperStatuses.xlsx");
             FileInfo file = new FileInfo(@"C:\TimeKeeper\TimeKeeper.xlsx");
-            using (ExcelPackage package = new ExcelPackage(file))
+            using (UnitOfWork unit = new UnitOfWork())
             {
-                using (UnitOfWork unit = new UnitOfWork())
-
+                using (ExcelPackage packageStatuses = new ExcelPackage(fileStatuses))
                 {
-
                     unit.Context.Database.EnsureDeleted();
                     unit.Context.Database.EnsureCreated();
+                    EmployeePositions.Collect(packageStatuses.Workbook.Worksheets["EmployeePosition"], unit);
+                    EmploymentStatuses.Collect(packageStatuses.Workbook.Worksheets["EmploymentStatus"], unit);
+                    DayTypes.Collect(packageStatuses.Workbook.Worksheets["DayType"], unit);
+                    CustomerStatuses.Collect(packageStatuses.Workbook.Worksheets["CustomerStatus"], unit);
+                    ProjectStatuses.Collect(packageStatuses.Workbook.Worksheets["ProjectStatus"], unit);
+                    PricingStatuses.Collect(packageStatuses.Workbook.Worksheets["PricingStatus"], unit);
 
+
+                }
+                using (ExcelPackage package = new ExcelPackage(file))
+                {
                     Teams.Collect(package.Workbook.Worksheets["Teams"], unit);
-
+                    Roles.Collect(package.Workbook.Worksheets["Roles"], unit);
+                    Customers.Collect(package.Workbook.Worksheets["Customers"], unit);
+                    Projects.Collect(package.Workbook.Worksheets["Projects"], unit);
                 }
             }
         }
