3ce4697cf4da921f50dd9ce920a3f437d7d8a47d
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-10-16 11:04:22 +0200

    Repositories in unito of work changed, console app library seed added

---
+++ TimeKeeper.Seed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeKeeper.Seed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
