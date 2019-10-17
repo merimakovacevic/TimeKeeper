22aecf8a06231044e5706121dc1588cfddcd925f
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-10-17 19:57:57 +0200

    Created API project

---
+++ TimeKeeper.API/Controllers/TeamsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeKeeper.DAL;
+
+namespace TimeKeeper.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private UnitOfWork unit;
+        public TeamsController(TimeKeeperContext context)
+        {
+            unit = new UnitOfWork(context);
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = unit.Teams.Get().Select(t=> new { Id=t.Id, Name=t.Name,
+                                                            Members=t.Members.Select(m=> new{m.Employee.FirstName, m.Role.Name}).ToList()}).ToList();
+            return Ok(result);
+        }
+        
+    }
+}
