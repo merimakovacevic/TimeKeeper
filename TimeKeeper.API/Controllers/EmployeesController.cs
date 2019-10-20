cf1f7a4060c78e1759cc5f700706ff50234f5c3c
Armin Odobasic <armindbsc@gmail.com>
2019-10-20 19:42:09 +0200

    Implemented all CRUD ops in EmployeesController

--- TimeKeeper.API/Controllers/EmployeesController.cs
+++ TimeKeeper.API/Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TimeKeeper.API.Factory;
 using TimeKeeper.DAL;
+using TimeKeeper.Domain.Entities;
 
 namespace TimeKeeper.API.Controllers
 {
@@ -18,14 +19,93 @@ namespace TimeKeeper.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var result = Unit.Employees.Get().ToList().Select(x => x.Create()).ToList();
-            return Ok(result);
+            try
+            {
+                return Ok(Unit.Employees.Get().ToList().Select(x => x.Create()).ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            var result = Unit.Employees.Get(id);
-            return Ok(result.Create());
+            try
+            {
+                Employee employee = Unit.Employees.Get(id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(employee.Create());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee employee)
+        {
+            try
+            {
+                employee.Status = Unit.EmploymentStatuses.Get(employee.Status.Id);
+                employee.Position = Unit.EmployeePositions.Get(employee.Position.Id);
+                Unit.Employees.Insert(employee);
+                Unit.Save();
+                return Ok(employee.Create());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                employee.Status = Unit.EmploymentStatuses.Get(employee.Status.Id);
+                employee.Position = Unit.EmployeePositions.Get(employee.Position.Id);
+                Unit.Employees.Update(employee, id);
+
+                int numberOfChanges = Unit.Save();
+
+                if(numberOfChanges == 0)
+                {
+                    return NotFound();
+                }
+                return Ok(employee.Create());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Unit.Employees.Delete(id);
+                int numberOfChanges = Unit.Save();
+                if(numberOfChanges == 0)
+                {
+                    return NotFound();
+                }
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
