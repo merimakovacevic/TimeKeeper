ef34d97143a25273bf949f227605e442b6d1e6ec
Armin Odobasic <armindbsc@gmail.com>
2019-10-20 19:59:08 +0200

    Implemented all CRUD ops in TeamsController

--- TimeKeeper.API/Controllers/TeamsController.cs
+++ TimeKeeper.API/Controllers/TeamsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TimeKeeper.API.Factory;
 using TimeKeeper.DAL;
+using TimeKeeper.Domain.Entities;
 
 namespace TimeKeeper.API.Controllers
 {
@@ -18,14 +19,93 @@ namespace TimeKeeper.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var result = Unit.Teams.Get().ToList().Select(x => x.Create()).ToList();
-            return Ok(result);
+            try
+            {
+                return Ok(Unit.Teams.Get().ToList().Select(x => x.Create()).ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
+
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            var result = Unit.Teams.Get(id);
-            return Ok(result.Create());
+            try
+            {
+                Team team = Unit.Teams.Get(id);
+                if (team == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(team.Create());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Team team)
+        {
+            try
+            {
+                Unit.Teams.Insert(team);
+                Unit.Save();
+                return Ok(team.Create());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Team team)
+        {
+            try
+            {
+                Unit.Teams.Update(team, id);
+                int numberOfChanges = Unit.Save();
+
+                if (numberOfChanges == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(team.Create());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Unit.Teams.Delete(id);
+
+                int numberOfChanges = Unit.Save();
+
+                if (numberOfChanges == 0)
+                {
+                    return NotFound();
+                }
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
