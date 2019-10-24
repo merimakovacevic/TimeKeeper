007deda4ed2102af3ec76f7576d9a0267bcca564
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-10-24 13:55:01 +0200

    adds .json files for services and teamMembers, adds teamCard that is optimised on mobile device and svg icons for social media

---
+++ TimeKeeper.Web/src/components/TeamPage/TeamCard/TeamCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import classes from "./TeamCard.module.css";
+import test from "../../../assets/backgundWithEffect.png";
+
+import LnIcon from "../../../assets/servicesSVGIcons/linkedin.svg";
+import GitIcon from "../../../assets/servicesSVGIcons/github-sign.svg";
+import FbIcon from "../../../assets/servicesSVGIcons/facebook.svg";
+
+const teamCard = props => (
+    <div className={classes.TeamCard}>
+        <img src={test} alt="" className={classes.Img} />
+        <h2 className={classes.Name}>Ime i Prezime</h2>
+        <p className={classes.Role}>Lorem</p>
+
+        <p className={classes.About}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eos aliquid adipisci eius
+            nemo libero vitae quibusdam recusandae.
+        </p>
+
+        <hr />
+
+        <div>
+            <a href={props.link}>
+                <img src={LnIcon} alt="" className={classes.Icon} />
+            </a>
+            <a href={props.link}>
+                <img src={GitIcon} alt="" className={classes.Icon} />
+            </a>
+            <a href={props.link}>
+                <img src={FbIcon} alt="" className={classes.Icon} />
+            </a>
+        </div>
+    </div>
+);
+
+export default teamCard;
