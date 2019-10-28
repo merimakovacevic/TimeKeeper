948830690e920065f1f801131e9789be4f4149c1
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-10-28 11:22:18 +0100

    adds fixes for vh of teamSection and servicesSection, refactoring footer

--- TimeKeeper.Web/src/containers/StaticPage/StaticPage.js
+++ TimeKeeper.Web/src/containers/StaticPage/StaticPage.js
@@ -5,14 +5,15 @@ import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import AboutPage from "../../components/AboutSection/AboutSection";
 import ServicesPage from "../../components/ServicesSection/ServicesSection";
 import TeamPage from "../../components/TeamSection/TeamSection";
+import Footer from "../../components/Footer/Footer.js";
 
 class StaticPage extends React.Component {
-  state = {
-    showSideDrawer: false
-    // screenWidth: document.body.offsetWidth
-  };
+    state = {
+        showSideDrawer: false
+        // screenWidth: document.body.offsetWidth
+    };
 
-  /* componentDidMount() {
+    /* componentDidMount() {
         this.updateWindowDimensions();
         window.addEventListener("resize", this.updateWindowDimensions);
     }
@@ -23,33 +24,33 @@ class StaticPage extends React.Component {
         this.setState({ screenWidth: window.innerWidth });
     };*/
 
-  sideDrawerClosedHandler = () => this.setState({ showSideDrawer: false });
+    sideDrawerClosedHandler = () => this.setState({ showSideDrawer: false });
 
-  drawerToggleClicked = () =>
-    this.setState(prevState => {
-      return { showSideDrawer: !prevState.showSideDrawer };
-    });
+    drawerToggleClicked = () =>
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
 
-  render() {
-    return (
-      <React.Fragment>
-        <Navigation ToggleButtonClicked={this.drawerToggleClicked} />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
-        <main>
-          <AboutPage passedId={"about"} />
-          <ServicesPage passedId={"services"} />
-          <TeamPage passedId={"team"} />
-          <div
-            id="contact"
-            style={{ height: "100vh", backgroundColor: "blue" }}
-          />
-        </main>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Navigation ToggleButtonClicked={this.drawerToggleClicked} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main>
+                    <AboutPage passedId={"about"} />
+                    <ServicesPage passedId={"services"} />
+                    <TeamPage passedId={"team"} />
+                    <div id="contact" style={{ height: "91vh", backgroundColor: "blue" }} />
+                </main>
+                <footer>
+                    <Footer />
+                </footer>
+            </React.Fragment>
+        );
+    }
 }
 
 export default StaticPage;
