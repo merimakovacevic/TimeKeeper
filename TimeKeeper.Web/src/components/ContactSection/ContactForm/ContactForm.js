5bbf1c594e44345edfc7d6a97de169badcdc3f4c
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-11-03 12:11:51 +0100

    adds minor bug fixes

--- TimeKeeper.Web/src/components/ContactSection/ContactForm/ContactForm.js
+++ TimeKeeper.Web/src/components/ContactSection/ContactForm/ContactForm.js
@@ -16,7 +16,7 @@ const ContactSchema = Yup.object().shape({
     name: Yup.string()
         .min(2, "Too Short!")
         .max(50, "Too Long!")
-        .required("Name isRequired"),
+        .required("Name is Required"),
     phone: Yup.string()
         .min(6, "Too Short!")
         .max(50, "Too Long!"),
