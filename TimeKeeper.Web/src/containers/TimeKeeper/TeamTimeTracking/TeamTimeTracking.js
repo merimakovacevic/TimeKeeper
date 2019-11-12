434b88e675778b09f8e0297c8a600d6b45d70332
Ajdin Zorlak <ajdinzo@mistral.ba>
2019-11-12 16:25:10 +0100

    Added and implemented dropdown menu to show teams from database

--- TimeKeeper.Web/src/containers/TimeKeeper/TeamTimeTracking/TeamTimeTracking.js
+++ TimeKeeper.Web/src/containers/TimeKeeper/TeamTimeTracking/TeamTimeTracking.js
@@ -20,52 +20,73 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Tooltip from "@material-ui/core/Tooltip";
-
+import DropDown from "./DropDown";
 import IconButton from "@material-ui/core/IconButton";
-import AddIcon from "@material-ui/icons/Add";
 
 let counter = 0;
-function createData(businessName, contactName, emailAddress, status) {
-    counter += 1;
-    return { id: counter, businessName, contactName, emailAddress, status };
+function createData(
+  employee,
+  workingHours,
+  businessAbsence,
+  publicHoliday,
+  vacation,
+  sickDays,
+  missingEntries
+) {
+  counter += 1;
+  return {
+    id: counter,
+    employee,
+    workingHours,
+    businessAbsence,
+    publicHoliday,
+    vacation,
+    sickDays,
+    missingEntries
+  };
 }
 
-const desc = (a, b, orderBy) => (b[orderBy] < a[orderBy] ? -1 : b[orderBy] > a[orderBy] ? 1 : 0);
+const desc = (a, b, orderBy) =>
+  b[orderBy] < a[orderBy] ? -1 : b[orderBy] > a[orderBy] ? 1 : 0;
 
 const stableSort = (array, cmp) => {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = cmp(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map(el => el[0]);
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
 };
 
 const getSorting = (order, orderBy) =>
-    order === "desc" ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+  order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
 
 const rows = [
-    { id: "employee", label: "Employee" },
-    { id: "workingHours", label: "Working hours" },
-    { id: "businessAbsence", label: "Business Absence" },
-    { id: "vacation", label: "Vacation" },
-    { id: "missingEntries", label: "Missing entries" },
-    { id: "actions", label: "" }
+  { id: "employee", label: "Employee" },
+  { id: "workingHours", label: "Working hours" },
+  { id: "businessAbsence", label: "Business Absence" },
+  { id: "publicHoliday", label: "Public Holiday" },
+  { id: "vacation", label: "Vacation" },
+  { id: "sickDays", label: "Sick Days" },
+  { id: "missingEntries", label: "Missing entries" },
+  { id: "actions", label: "Actions" }
 ];
 
 class EnhancedTable extends React.Component {
-    state = {
-        loading: null,
-        order: "asc",
-        orderBy: "employee",
-        selected: [],
-        data: [],
-        rowsPerPage: 6,
-        page: 0
-    };
+  state = {
+    loading: null,
+    order: "asc",
+    orderBy: "employee",
+    selected: [],
+    data: [],
+    rowsPerPage: 6,
+    page: 0
+  };
 
-    /* componentDidMount() {
+  /* componentDidMount() {
         this.setState({ loading: true });
         axios(`${config.apiUrl}customers`, {
             headers: {
@@ -82,162 +103,150 @@ class EnhancedTable extends React.Component {
             .catch(err => this.setState({ loading: false }));
     } */
 
-    handleRequestSort = property => {
-        const orderBy = property;
-        let order = "desc";
+  handleRequestSort = property => {
+    const orderBy = property;
+    let order = "desc";
 
-        this.state.orderBy === property && this.state.order === "desc"
-            ? (order = "asc")
-            : (order = "desc");
+    this.state.orderBy === property && this.state.order === "desc"
+      ? (order = "asc")
+      : (order = "desc");
 
-        this.setState({ order, orderBy });
-    };
+    this.setState({ order, orderBy });
+  };
 
-    handleChangePage = (event, page) => this.setState({ page });
+  handleChangePage = (event, page) => this.setState({ page });
 
-    isSelected = id => this.state.selected.indexOf(id) !== -1;
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
 
-    render() {
-        const { classes } = this.props;
-        const { data, order, orderBy, rowsPerPage, page, loading } = this.state;
+  render() {
+    const { classes } = this.props;
+    const { data, order, orderBy, rowsPerPage, page, loading } = this.state;
 
-        return (
-            <React.Fragment>
-                {loading ? (
-                    <Backdrop open={true}>
-                        <div className={classNames(classes.center)}>
-                            <CircularProgress size={100} className={classNames(classes.loader)} />
-                            <h1 className={classNames(classes.loaderText)}>Loading...</h1>
-                        </div>
-                    </Backdrop>
-                ) : (
-                    <Paper className={classes.root}>
-                        <Toolbar className={classNames(classes.root2, {})}>
-                            <div className={classes.title}>
-                                <Typography variant="h4" id="tableTitle">
-                                    Customers
-                                </Typography>
-                            </div>
-                            <div className={classes.spacer} />
-                            <div className={classes.actions}>
-                                <Tooltip title="Add">
-                                    <IconButton aria-label="Add">
-                                        <AddIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </div>
-                        </Toolbar>
-                        <div className={classes.tableWrapper}>
-                            <Table className={classes.table} aria-labelledby="tableTitle">
-                                <TableHead>
-                                    <TableRow>
-                                        {rows.map(
-                                            row => (
-                                                <TableCell
-                                                    className={classNames(classes.tableCell)}
-                                                    key={row.id}
-                                                    sortDirection={
-                                                        orderBy === row.id ? order : false
-                                                    }
-                                                >
-                                                    <Tooltip
-                                                        title="Sort"
-                                                        placement={
-                                                            row.numeric
-                                                                ? "bottom-end"
-                                                                : "bottom-start"
-                                                        }
-                                                        enterDelay={150}
-                                                    >
-                                                        <TableSortLabel
-                                                            active={orderBy === row.id}
-                                                            direction={order}
-                                                            onClick={() =>
-                                                                this.handleRequestSort(row.id)
-                                                            }
-                                                        >
-                                                            {row.label}
-                                                        </TableSortLabel>
-                                                    </Tooltip>
-                                                </TableCell>
-                                            ),
-                                            this
-                                        )}
-                                    </TableRow>
-                                </TableHead>
+    return (
+      <React.Fragment>
+        {loading ? (
+          <Backdrop open={true}>
+            <div className={classNames(classes.center)}>
+              <CircularProgress
+                size={100}
+                className={classNames(classes.loader)}
+              />
+              <h1 className={classNames(classes.loaderText)}>Loading...</h1>
+            </div>
+          </Backdrop>
+        ) : (
+          <Paper className={classes.root}>
+            <Toolbar className={classNames(classes.root2, {})}>
+              <div className={classes.title}>
+                <Typography variant="h4" id="tableTitle">
+                  Team tracking
+                </Typography>
+              </div>
+              <DropDown></DropDown>
+              <div className={classes.spacer} />
+              <div className={classes.actions}></div>
+            </Toolbar>
+            <div className={classes.tableWrapper}>
+              <Table className={classes.table} aria-labelledby="tableTitle">
+                <TableHead>
+                  <TableRow>
+                    {rows.map(
+                      row => (
+                        <TableCell
+                          className={classNames(classes.tableCell)}
+                          key={row.id}
+                          sortDirection={orderBy === row.id ? order : false}
+                        >
+                          <Tooltip
+                            title="Sort"
+                            placement={
+                              row.numeric ? "bottom-end" : "bottom-start"
+                            }
+                            enterDelay={150}
+                          >
+                            <TableSortLabel
+                              active={orderBy === row.id}
+                              direction={order}
+                              onClick={() => this.handleRequestSort(row.id)}
+                            >
+                              {row.label}
+                            </TableSortLabel>
+                          </Tooltip>
+                        </TableCell>
+                      ),
+                      this
+                    )}
+                  </TableRow>
+                </TableHead>
 
-                                <TableBody>
-                                    {stableSort(data, getSorting(order, orderBy))
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        .map(n => {
-                                            const isSelected = this.isSelected(n.id);
-                                            return (
-                                                <TableRow
-                                                    hover
-                                                    onClick={event => this.handleClick(event, n.id)}
-                                                    tabIndex={-1}
-                                                    key={n.id}
-                                                    selected={isSelected}
-                                                >
-                                                    <TableCell component="th" scope="row">
-                                                        {n.employee}
-                                                    </TableCell>
-                                                    <TableCell className={classes.twoWordLabel}>
-                                                        {n.workingHours}
-                                                    </TableCell>
-                                                    <TableCell className={classes.twoWordLabel}>
-                                                        {n.businessAbsence}
-                                                    </TableCell>
-                                                    <TableCell>{n.vacation}</TableCell>
-                                                    <TableCell className={classes.twoWordLabel}>
-                                                        {n.missingEntries}
-                                                    </TableCell>
+                <TableBody>
+                  {stableSort(data, getSorting(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map(n => {
+                      const isSelected = this.isSelected(n.id);
+                      return (
+                        <TableRow
+                          hover
+                          onClick={event => this.handleClick(event, n.id)}
+                          tabIndex={-1}
+                          key={n.id}
+                          selected={isSelected}
+                        >
+                          <TableCell component="th" scope="row">
+                            {n.employee}
+                          </TableCell>
+                          <TableCell>{n.workingHours}</TableCell>
+                          <TableCell>{n.businessAbsence}</TableCell>
+                          <TableCell>{n.publicHoliday}</TableCell>
+                          <TableCell>{n.vacation}</TableCell>
+                          <TableCell>{n.sickDays}</TableCell>
+                          <TableCell>{n.missingEntries}</TableCell>
 
-                                                    <TableCell align="center">
-                                                        {" "}
-                                                        <ButtonGroup>
-                                                            <Button
-                                                                variant="outlined"
-                                                                size="small"
-                                                                color="primary"
-                                                            >
-                                                                View
-                                                            </Button>
-                                                            <Button
-                                                                variant="outlined"
-                                                                size="small"
-                                                                color="primary"
-                                                            >
-                                                                Edit
-                                                            </Button>
-                                                        </ButtonGroup>
-                                                    </TableCell>
-                                                </TableRow>
-                                            );
-                                        })}
-                                </TableBody>
-                            </Table>
-                        </div>
-                        <TablePagination
-                            component="div"
-                            count={data.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            backIconButtonProps={{
-                                "aria-label": "Previous Page"
-                            }}
-                            nextIconButtonProps={{
-                                "aria-label": "Next Page"
-                            }}
-                            onChangePage={this.handleChangePage}
-                            labelRowsPerPage=""
-                            rowsPerPageOptions={[]}
-                        />
-                    </Paper>
-                )}
-            </React.Fragment>
-        );
-    }
+                          <TableCell align="center">
+                            {" "}
+                            <ButtonGroup>
+                              <Button
+                                variant="outlined"
+                                size="small"
+                                color="primary"
+                              >
+                                View
+                              </Button>
+                              <Button
+                                variant="outlined"
+                                size="small"
+                                color="primary"
+                              >
+                                Edit
+                              </Button>
+                            </ButtonGroup>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </div>
+            <TablePagination
+              component="div"
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              backIconButtonProps={{
+                "aria-label": "Previous Page"
+              }}
+              nextIconButtonProps={{
+                "aria-label": "Next Page"
+              }}
+              onChangePage={this.handleChangePage}
+              labelRowsPerPage=""
+              rowsPerPageOptions={[]}
+            />
+          </Paper>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(EnhancedTable);
