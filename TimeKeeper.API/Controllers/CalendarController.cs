ebe4195924f0167ab9d901b85cb2ab0c62c958e4
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-11-14 17:14:35 +0100

    Solved conflicts

--- TimeKeeper.API/Controllers/CalendarController.cs
+++ TimeKeeper.API/Controllers/CalendarController.cs
@@ -200,20 +200,5 @@ namespace TimeKeeper.API.Controllers
                 return HandleException(ex);
             }
         }
-
-        [HttpGet("team-time-tracking/{teamId}/{year}/{month}")]
-        public IActionResult GetTimeTracking(int teamId, int year, int month)
-        {
-            try
-            {
-                return Ok(teamCalendarService.TeamMonthReport(teamId, month, year));
-                //return Ok(TeamCalendarService.TeamMonthReport(teamId, month, year));
-            }
-            catch (Exception ex)
-            {
-                Logger.Fatal(ex);
-                return BadRequest(ex);
-            }
-        }
     }
 }
