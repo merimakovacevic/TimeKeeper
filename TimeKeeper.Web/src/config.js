eb76e92cae8b49545d25044d107654a22bdf2770
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-11-14 20:13:44 +0100

    fixing some minor bugs

--- TimeKeeper.Web/src/config.js
+++ TimeKeeper.Web/src/config.js
@@ -1,6 +1,6 @@
 export default {
-  token: "Basic bWljaGFlbGpvOiRjaDAwbA==",
-  // apiUrl: "http://192.168.60.73/TimeKeeper/api/",
-   apiUrl: "http://localhost:63990/api/",
+  token: "",
+   apiUrl: "http://192.168.60.73/TimeKeeper/api/",
+   //apiUrl: "http://localhost:63990/api/",
   authHeader: {}
 };
