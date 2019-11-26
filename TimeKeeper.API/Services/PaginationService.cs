16990142c0b0c69c472261276b8ffce3aa1ea858
Armin Odobasic <armindbsc@gmail.com>
2019-11-26 21:14:41 +0100

    Changed PaginationService argumetn from DbSet to Enumerable, made changes to EmployeesController pagination implementation, implemented pagination in CustomersController

--- TimeKeeper.API/Services/PaginationService.cs
+++ TimeKeeper.API/Services/PaginationService.cs
@@ -9,24 +9,49 @@ namespace TimeKeeper.API.Services
 {
     public class PaginationService<Entity> where Entity:class
     {
-        public Tuple<PaginationModel, List<Entity>> CreatePagination(int page, int pageSize, DbSet<Entity> dbSet)
+        public Tuple<PaginationModel, List<Entity>> CreatePagination(int page, int pageSize, IEnumerable<Entity> data)
         {
-            int totalItems = dbSet.Count();
+            int totalItems = data.Count();
             int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+
             if (page < 1) page = 1;
             if (page > totalPages) page = totalPages;
+
             int currentPage = page - 1;
-            var query = dbSet.Skip(currentPage * pageSize).Take(pageSize);
+            var query = data.Skip(currentPage * pageSize).Take(pageSize);
+
             return new Tuple<PaginationModel, List<Entity>>
                 (
                     new PaginationModel
                     {
-                        PageSize=pageSize,
-                        TotalItems=totalItems,
-                        TotalPages=totalPages,
-                        Page=page
+                        PageSize = pageSize,
+                        TotalItems = totalItems,
+                        TotalPages = totalPages,
+                        Page = page
                     },
-                    query.ToList());
+                    query.ToList()
+                );
         }
+
+        /*public Tuple<PaginationModel, List<Entity>> CreatePagination(int page, int pageSize, IList<Entity> list)
+        {
+            int totalItems = list.Count();
+            int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+            if (page < 1) page = 1;
+            if (page > totalPages) page = totalPages;
+            int currentPage = page - 1;
+            var query = list.Skip(currentPage * pageSize).Take(pageSize);
+            return new Tuple<PaginationModel, List<Entity>>
+                (
+                    new PaginationModel
+                    {
+                        PageSize = pageSize,
+                        TotalItems = totalItems,
+                        TotalPages = totalPages,
+                        Page = page
+                    },
+                    query.ToList()
+                );
+        }*/
     }
 }
