3e875f2dc1c28986ea6215e1e58382d2a3447cda
merimakovacevic <mkovacevic1@etf.unsa.ba>
2019-11-28 16:03:20 +0100

    Created methods for annual overview and project history

---
+++ TimeKeeper.API/Models/ProjectAnnualOverviewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.API.Models
+{
+    public class ProjectAnnualOverviewModel
+    {
+        public ProjectAnnualOverviewModel()
+        {
+            Months = new Dictionary<int, decimal>();
+        }
+        public MasterModel Project { get; set; }
+        public Dictionary<int, decimal> Months { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ TimeKeeper.API/Models/TotalAnnualOverviewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.API.Models
+{
+    public class TotalAnnualOverviewModel
+    {
+        public TotalAnnualOverviewModel()
+        {
+            Months = new Dictionary<int, decimal>();
+        }
+        public List<ProjectAnnualOverviewModel> Projects { get; set; }
+        public Dictionary<int, decimal> Months { get; set; }
+        public decimal SumTotal { get; set; }
+    }
+}
