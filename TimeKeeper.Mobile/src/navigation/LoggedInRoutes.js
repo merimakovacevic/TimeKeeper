6fdbbcfab710d1aa40549faf66b9f973924901c4
Ajdin Zorlak <ajdinzo@mistral.ba>
2019-12-10 10:51:41 +0100

    Positioned calendar (agenda) properly

--- TimeKeeper.Mobile/src/navigation/LoggedInRoutes.js
+++ TimeKeeper.Mobile/src/navigation/LoggedInRoutes.js
@@ -9,46 +9,58 @@ import Calendar from "../views/Calendar";
 import People from "../views/People";
 import Customers from "../views/Customers";
 import Projects from "../views/Projects";
+import Agenda from "../views/Agenda";
 
 const StackNavigator = createStackNavigator({
-	Profile: {
-		screen: Profile
-	},
-	Calendar: {
-		screen: Calendar
-	}
+  Profile: {
+    screen: Profile
+  },
+  Calendar: {
+    screen: Calendar
+  }
 });
 
 const DrawerNavigator = createDrawerNavigator({
-	People: {
-		screen: People
-	},
-	Customers: {
-		screen: Customers
-	},
-	Projects: {
-		screen: Projects
-	}
+  People: {
+    screen: People
+  },
+  Customers: {
+    screen: Customers
+  },
+  Projects: {
+    screen: Projects
+  },
+  Agenda: {
+    screen: Agenda
+  }
 });
 
 const LoggedInRoutes = createBottomTabNavigator(
-	{
-		People: {
-			screen: DrawerNavigator,
-			navigationOptions: {
-				tabBarIcon: ({ tintColor }) => <Ionicons name="ios-people" size={40} color={tintColor} />
-			}
-		},
-		Profile: {
-			screen: StackNavigator,
-			navigationOptions: {
-				tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={32} color={tintColor} />
-			}
-		}
-	},
-	{
-		tabBarOptions: { showLabel: false, activeTintColor: "#32aedc", inactiveTintColor: "gray" }
-	}
+  {
+    People: {
+      screen: DrawerNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-people" size={40} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: StackNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-person" size={32} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: "#32aedc",
+      inactiveTintColor: "gray"
+    }
+  }
 );
 
 export default LoggedInRoutes;
