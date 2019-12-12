3413783f81781a89d360b3ed27c63f45609e0181
Armin Odobasic <armindbsc@gmail.com>
2019-12-12 23:03:14 +0100

    Changed authentication to TokenAuthentication in Dashboard, Report and Calendar controller

--- TimeKeeper.API/Controllers/ReportsController.cs
+++ TimeKeeper.API/Controllers/ReportsController.cs
@@ -10,7 +10,7 @@ using TimeKeeper.DAL;
 
 namespace TimeKeeper.API.Controllers
 {
-    //[Authorize]
+    [Authorize(AuthenticationSchemes = "TokenAuthentication")]
     [Route("api/[controller]")]
     [ApiController]
     public class ReportsController : BaseController
